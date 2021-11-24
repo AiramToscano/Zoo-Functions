72c526780bb07ce1b15d977114ef2ede14844d2c
Airam Toscano <airamlobato@gmail.com>
2021-11-24 15:37:38 -0400

    Exercicio 1 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  // o map criar um novo array, com parametros ids e o find proucura meu id que eu desejo.
+  return ids.map((id) => data.species.find((animal) => animal.id === id));
 }
-
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
