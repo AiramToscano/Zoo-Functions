731a11078d722ca6f2e1c15ebdd2891dd5451aa9
Airam Toscano <airamlobato@gmail.com>
2021-11-24 17:05:18 -0400

    Exercicio 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.find((animal1) => animal1.name === animal)
+    .residents.every((year) => year.age >= age);
 }
-
+// console.log(getAnimalsOlderThan('lions',10));
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  // o map criar um novo array, com parametros ids e o find proucura meu id que eu desejo.
+// o map criar um novo array, com parametros ids e o find proucura meu id que eu desejo.
+// caso não passe nenhum parametro para meu map, ele retorna um array em branco.
   return ids.map((id) => data.species.find((animal) => animal.id === id));
 }
-console.log(getSpeciesByIds());
+// console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
